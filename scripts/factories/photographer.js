0bccaf1424ac938d43be96913f55efe2677c9150
qgrems <quentin.gremaud@gmail.com>
2022-03-24 19:32:44 +0100

    Arrondi des photos de la page index, ajout des elements de description de chaques photographes

--- scripts/factories/photographer.js
+++ scripts/factories/photographer.js
@@ -1,5 +1,5 @@
 function photographerFactory(data) {
-    const { name, portrait } = data;
+    const { name, portrait,city,country,tagline,price } = data;
 
     const picture = `assets/photographers/${portrait}`;
 
@@ -9,8 +9,18 @@ function photographerFactory(data) {
         img.setAttribute("src", picture)
         const h2 = document.createElement( 'h2' );
         h2.textContent = name;
+        const city_country = document.createElement('h3')
+            city_country.textContent = `${city}, ${country} `;
+        const tagLine = document.createElement('h4');
+        tagLine.textContent = tagline;
+        const _price = document.createElement('price');
+        _price.textContent = `${price}/jour` ;
+
         article.appendChild(img);
         article.appendChild(h2);
+        article.appendChild(city_country);
+        article.appendChild(tagLine);
+        article.appendChild(_price)
         return (article);
     }
 
