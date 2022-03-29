873e62539f01e8b7b94615cb8b06c26a3d2b476a
qgrems <quentin.gremaud@gmail.com>
2022-03-29 03:12:44 +0200

    ajout des fichiers liens photos et prenoms

--- scripts/factories/photographer.js
+++ scripts/factories/photographer.js
@@ -1,28 +1,47 @@
+
+
 function photographerFactory(data) {
-    const { name, portrait,city,country,tagline,price } = data;
+    const { name, portrait,city,country,tagline,price,id } = data;
 
     const picture = `assets/photographers/${portrait}`;
-
     function getUserCardDOM() {
-        const article = document.createElement( 'article' );
-        const img = document.createElement( 'img' );
-        img.setAttribute("src", picture)
-        const h2 = document.createElement( 'h2' );
+
+        const article = document.createElement('article');
+
+        const img = document.createElement('img' )
+        img.setAttribute("src", picture);
+
+        const a = document.createElement('a')
+        a.setAttribute("href", "photographer.html");
+
+        const h2 = document.createElement( 'h2');
         h2.textContent = name;
+
+        const b = document.createElement('a')
+
+        b.setAttribute("href", "photographer.html");
+
         const city_country = document.createElement('h3')
-            city_country.textContent = `${city}, ${country} `;
+        city_country.textContent = `${city}, ${country} `;
+
         const tagLine = document.createElement('h4');
         tagLine.textContent = tagline;
+
         const _price = document.createElement('price');
         _price.textContent = `${price}/jour` ;
 
-        article.appendChild(img);
-        article.appendChild(h2);
+        a.appendChild(img);
+        article.appendChild(a);
+
+        b.appendChild(h2);
+        article.appendChild(b);
+
         article.appendChild(city_country);
         article.appendChild(tagLine);
-        article.appendChild(_price)
+        article.appendChild(_price);
+       
         return (article);
+        
     }
-
     return { name, picture, getUserCardDOM }
 }
