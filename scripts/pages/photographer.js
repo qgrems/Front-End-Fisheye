95ae6073aa528e0be26eb9c2c3a7443c6750b6b8
qgrems <quentin.gremaud@gmail.com>
2022-05-11 20:20:13 +0200

    fonction sort pour filtre fonctionne mais ne supprime pas le precedent affichage

--- scripts/pages/photographer.js
+++ scripts/pages/photographer.js
@@ -55,81 +55,93 @@ class App {
         this.tri.appendChild(triage.render())
 
   
-        let value = 0;    
+        let value = 0;
+        var med = medias
         const triage2 = document.getElementById('valuetriage')
-       
-       
+        triage2.addEventListener('change', (media,mediaSection)=>{
+            let model
+            value = triage2.value 
+            if (value ===  "1")
+            {
+                console.log(value)
+                med = medias.sort((a,b)=> a.likes - b.likes)
+                med.forEach((media) => {
+                    let mediaModel;
+        
+                    if (media.video) 
+                    {
+                        mediaModel = new multimedia(media, photographer[0], sliceName, 'video')
+                    }
+                    else 
+                    {
+                        mediaModel = new multimedia(media, photographer[0], sliceName, 'image')
+                    }
+                    this.mediaSection.appendChild(mediaModel.render());
+                    
+                      tabMedia.push(mediaModel)
+                });
+            }
+            
+            if (value ===  '2')
+            {
+                 med = medias.sort(function(a, b){
+                     return a.date.localeCompare(b.date);
+                 })
+                 med.forEach((media) => {
+                    let mediaModel;
+        
+                    if (media.video) 
+                    {
+                        mediaModel = new multimedia(media, photographer[0], sliceName, 'video')
+                    }
+                    else 
+                    {
+                        mediaModel = new multimedia(media, photographer[0], sliceName, 'image')
+                    }
+                    this.mediaSection.appendChild(mediaModel.render());
+                    
+                      tabMedia.push(mediaModel)
+                });
+            }
+
+            if (value ===  '3')
+            {
+                med =  medias.sort(function(a, b){
+                    return a.title.localeCompare(b.title);
+                })
+                med.forEach((media) => {
+                    let mediaModel;
+        
+                    if (media.video) 
+                    {
+                        mediaModel = new multimedia(media, photographer[0], sliceName, 'video')
+                    }
+                    else 
+                    {
+                        mediaModel = new multimedia(media, photographer[0], sliceName, 'image')
+                    }
+                    this.mediaSection.appendChild(mediaModel.render());
+                    
+                      tabMedia.push(mediaModel)
+                });
+            }
+        }) 
+
 
         medias.forEach((media) => {
             let mediaModel;
 
             if (media.video) 
             {
-                mediaModel = multimedia(media, photographer[0], sliceName, 'video')
+                mediaModel = new multimedia(media, photographer[0], sliceName, 'video')
             }
             else 
             {
-                mediaModel = multimedia(media, photographer[0], sliceName, 'image')
+                mediaModel = new multimedia(media, photographer[0], sliceName, 'image')
             }
             this.mediaSection.appendChild(mediaModel.render());
-           
-            triage2.addEventListener('change', (media,mediaSection)=>{
-                value = triage2.value 
-                let model
-                if (value ===  '2')
-                {
-                    const sortLikes = medias.sort((a,b)=> a.likes - b.likes)
-                    sortLikes.forEach((media) => {
-                    if (media.video) 
-                    {
-                        model = multimedia(media, photographer[0], sliceName, 'video')
-                    }
-                    else 
-                    {
-                        model = multimedia(media, photographer[0], sliceName, 'image')
-                    }
-                    this.mediaSection.appendChild(model.render());
-                 })
-                 
-                }
-                if (value ===  '3')
-                {
-                    const sortData = medias.sort(function(a, b){
-                        return a.date.localeCompare(b.date);
-                    })
-                    sortData.forEach((media) => {
-                        if (media.video) 
-                        {
-                            mediaModel = multimedia(media, photographer[0], sliceName, 'video')
-                        }
-                        else 
-                        {
-                            mediaModel = multimedia(media, photographer[0], sliceName, 'image')
-                        }
-                    })
-                    this.mediaSection.appendChild(mediaModel.render());
-                }
-                if (value ===  '4')
-                {
-                    const sortName = medias.sort(function(a, b){
-                        return a.title.localeCompare(b.title);
-                    })
-                    sortName.forEach((media) => {
-                        if (media.video) 
-                        {
-                            mediaModel = multimedia(media, photographer[0], sliceName, 'video')
-                        }
-                        else 
-                        {
-                            mediaModel = multimedia(media, photographer[0], sliceName, 'image')
-                        }
-                    })
-                    this.mediaSection.appendChild(mediaModel.render());
-                }
-                
-                tabMedia.push(mediaModel)
-            })
-
+            
+              tabMedia.push(mediaModel)
         });
 
         this.likesMedia = new IncrementationLikes(photographerModel,tabMedia,sumLikes)
--- scripts/utils/triage.js
+++ scripts/utils/triage.js
@@ -1,12 +1,82 @@
+medias.forEach((media) => {
+    let mediaModel;
 
-function triage(medias)
-{
-    const sortName = medias.sort(function(a, b){
-        return a.title.localeCompare(b.title);
-    })
-    const sortData = medias.sort(function(a, b){
-        return a.data.localeCompare(b.data);
-    })
-    console.log(sortName)
+    if (media.video) 
+    {
+        mediaModel = multimedia(media, photographer[0], sliceName, 'video')
+    }
+    else 
+    {
+        mediaModel = multimedia(media, photographer[0], sliceName, 'image')
+    }
+    this.mediaSection.appendChild(mediaModel.render());
+    const triage2 = document.getElementById('valuetriage')
+    triage2.addEventListener('change', (media,mediaSection)=>{
+        let model
+        value = triage2.value 
+        if (value ===  "1")
+        {
+            console.log(value)
+            const sortLikes = medias.sort((a,b)=> a.likes - b.likes)
+            sortLikes.forEach((media) => {
+            if (media.video) 
+            {
+                model = multimedia(media, photographer[0], sliceName, 'video')
+            }
+            else 
+            {
+                model = multimedia(media, photographer[0], sliceName, 'image')
+                
+            }
+            this.mediaSection.appendChild(model.render());
+            })
+        }
+        
+         if (value ===  '2')
+         {
+             const sortData = medias.sort(function(a, b){
+                 return a.date.localeCompare(b.date);
+             })
+             sortData.forEach((media) => {
+                 if (media.video) 
+                 {
+                     mediaModel = multimedia(media, photographer[0], sliceName, 'video')
+                 }
+                 else 
+                 {
+                     mediaModel = multimedia(media, photographer[0], sliceName, 'image')
+                 }
+             })
+        }
+
+        if (value ===  '3')
+        {
+
+            const sortName = medias.sort(function(a, b){
+                return a.title.localeCompare(b.title);
+            })
+            sortName.forEach((media) => {
+                if (media.video) 
+                {
+                    mediaModel = multimedia(media, photographer[0], sliceName, 'video')
+                }
+                else 
+                {
+                    mediaModel = multimedia(media, photographer[0], sliceName, 'image')
+                }
+            })
+            this.mediaSection.appendChild(mediaModel.render());
+        }
+    }) 
+      tabMedia.push(mediaModel)
+});
+
+this.likesMedia = new IncrementationLikes(photographerModel,tabMedia,sumLikes)
+this.modalMedia = new ModalMedia(photographerModel, tabMedia)
+
+}
 }
 
+
+const app = new App()
+app.main();
